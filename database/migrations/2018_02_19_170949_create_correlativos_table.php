273812f8e1de6ee89af2b13d801cbbd6b027921d
Edwin Ajahuanca Callisaya <edwinajahuancacallisaya@gmail.com>
2018-02-19 17:14:53 -0400

    new files test

--- database/migrations/2018_02_19_170949_create_correlativos_table.php
+++ database/migrations/2018_02_19_170949_create_correlativos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePermisosTable extends Migration
+class CreateCorrelativosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePermisosTable extends Migration
      */
     public function up()
     {
-        Schema::create('permisos', function (Blueprint $table) {
+        Schema::create('correlativos', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreatePermisosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('permisos');
+        Schema::dropIfExists('correlativos');
     }
 }
