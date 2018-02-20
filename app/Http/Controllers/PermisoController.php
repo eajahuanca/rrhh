59143001b59edf68f4de839f9a8e24aeed34ac91
Edwin Ajahuanca <edwinajahuancacallisaya@gmail.com>
2018-02-20 10:09:10 -0400

    test controller

--- app/Http/Controllers/PermisoController.php
+++ app/Http/Controllers/PermisoController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class PermisoController extends Controller
 {
-    //
+    public function __construct(){
+        Carbon::setLocale('es');
+    }
+
+    public function index(){
+        return 'test';
+    }
 }
