0824e52eba65c6dbb70f7a9a2d6246579404e96b
Edwin Ajahuanca Callisaya <edwinajahuancacallisaya@gmail.com>
2018-03-05 13:59:50 -0400

    formulario de permisos con ckeditor+radio+checkbox

--- resources/views/admin/permiso/create.blade.php
+++ resources/views/admin/permiso/create.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.init')
 
 @section('styles')
+    <link rel="stylesheet" href="{{ asset('plugin/assets/css/style_radio_check.css') }}">
+    <link rel="stylesheet" href="">
 @endsection
 
 @section('actual','Permisos y Comisiones')
@@ -11,18 +13,28 @@
     <div class="table-header">
         Formulario de Registro
     </div><br>
+    
     {!! Form::open(['route' => 'permiso.store', 'method' => 'POST']) !!}
-    @include('admin.permiso.form')    
     <div class="row">
-        <center>
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
             <input type="submit" name="btnCancelar" value="Cancelar Permiso" class="btn btn-danger btn-round pull center">
             <input type="submit" name="btnPendiente" value="Pendiente" class="btn btn-warning btn-round pull center">
             <input type="submit" name="btnSolicitar" value="Solicitar Permiso" class="btn btn-success btn-round pull center">
-        </center>
+        </div>
+        <div class="col-md-1"></div>
     </div>
+    @include('admin.permiso.form')    
     {!! Form::close() !!}
 @endsection
 
 @section('scripts')
+    <script type="text/javascript" src="{{ asset('plugin/checkeditor/ckeditor.js') }}"></script>
+    <script>
+        CKEDITOR.replace( 'pre_motivo', {
+            language: 'es',
+            uiColor:  '#9AB8F3'
+        });
+    </script>
 @endsection
     
--- resources/views/admin/permiso/form.blade.php
+++ resources/views/admin/permiso/form.blade.php
@@ -3,66 +3,93 @@
 	<div class="col-md-10">
 		<div class="well well-lg">
 			<?php error_reporting(0); ?>
-			<h3>Seleccione el tipo de permiso</h3>
+			<h3>Seleccione el tipo de permiso</h3><br>
 			<div class="row">
-				<div class="col-md-3"><label>{!! Form::radio('check_per', 1, null,['class' => 'flat-red']) !!} COMISIÓN </label>
-					@if($errors->has('check_per'))
-						<span style="color:red;">
-							<strong>{!! $errors->first('check_per') !!}</strong>
-						</span>
-					@endif
+				<div class="col-md-1"></div>
+				<div class="col-md-3">
+					{!! Form::radio('pre_tipo', 1, null,['id' => 'r1']) !!}
+					<label for="r1"><span><span><div style="margin-left:4em;margin-top:0.4em;"> Comisión</div></label>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-3"><label>{!! Form::radio('check_per', 1, null,['class' => 'flat-red']) !!} PERSONAL</label>
-					@if($errors->has('check_per'))
-						<span style="color:red;">
-							<strong>{!! $errors->first('check_per') !!}</strong>
-						</span>
-					@endif
+				<div class="col-md-3">
+					{!! Form::radio('pre_tipo', 1, null,['id' => 'r2']) !!}
+					<label for="r2"><span><span><div style="margin-left:4em;margin-top:0.4em;"> Personal</div></label>
 				</div>
-				
-			</div>
-			<div class="row">
-				<div class="col-md-3"><label>{!! Form::radio('check_per', 1, null,['class' => 'flat-red']) !!} OTROS</label>
-					@if($errors->has('check_per'))
-						<span style="color:red;">
-							<strong>{!! $errors->first('check_per') !!}</strong>
-						</span>
-					@endif
+				<div class="col-md-3">
+					{!! Form::radio('pre_tipo', 1, null,['id' => 'r3']) !!}
+					<label for="r3"><span><span><div style="margin-left:4em;margin-top:0.4em;"> Otros</div></label>
 				</div>
-				
+				<div class="col-md-2"></div>
+				@if($errors->has('pre_tipo'))
+					<span style="color:red;">
+						<strong>{!! $errors->first('pre_tipo') !!}</strong>
+					</span>
+				@endif
 			</div>
+			<br>
 			<div class="row">
+				<div class="col-md-1"></div>
                 <div class="col-md-3">
-                    {!! Form::label('check_per','FECHA DEL PERMISO') !!}
+                    {!! Form::label('per_fechapermiso','Fecha de Permiso') !!}
                     <div class="input-group">
-                        <div class="input-group-addon">
+                        <div class="input-group-addon bg-info">
                             <i class="fa fa-calendar"></i>
                         </div>
-                        {!! Form::date('check_per', 1, null,['class' => 'flat-red']) !!}
+                        {!! Form::date('per_fechapermiso', 1, null) !!}
                     </div>
-					@if($errors->has('check_per'))
+					@if($errors->has('per_fechapermiso'))
 						<span style="color:red;">
-							<strong>{!! $errors->first('check_per') !!}</strong>
+							<strong>{!! $errors->first('per_fechapermiso') !!}</strong>
 						</span>
 					@endif
 				</div>
-				<div class="col-md-8"><label>DE HORAS: {!! Form::time('pre_horasal') !!}</label> <label>A HORAS: {!! Form::time('pre_horaretorno') !!}</label>  <label>
-                    S/R: {!! Form::checkbox('pre_sinretorno') !!}</label>
+				<div class="col-md-2">
+					{!! Form::label('per_horasalida','De Horas') !!}
+					<div class="input-group">
+						<div class="input-group-addon bg-info">
+							<i class="fa fa-safari"></i>
+						</div>
+						{!! Form::time('per_horasalida', 1, null) !!}
+					</div>
+					@if($errors->has('per_horasalida'))
+						<span style="color:red;">
+							<strong>{!! $errors->first('per_horasalida') !!}</strong>
+						</span>
+					@endif
 				</div>
-				<div class="col-md-8">
-					
+				<div class="col-md-2">
+					{!! Form::label('per_horaretorno','De Horas') !!}
+					<div class="input-group">
+						<div class="input-group-addon bg-info">
+							<i class="fa fa-safari"></i>
+						</div>
+						{!! Form::time('per_horaretorno', 1, null) !!}
+					</div>
+					@if($errors->has('per_horaretorno'))
+						<span style="color:red;">
+							<strong>{!! $errors->first('per_horaretorno') !!}</strong>
+						</span>
+					@endif
+				</div>
+				<div class="col-md-2">
+					<br>
+					{!! Form::checkbox('pre_sinretorno', null, null,['id' => 'c2']) !!}
+					<label for="c2"><span><span><div style="margin-left:4em;margin-top:0.4em;"> Sin/Retorno</div></label>
+				</div>
+			</div><br>
+			<div class="row">
+				<div class="col-md-1"></div>
+				<div class="col-md-10">
 					<div class="form-group {!! $errors->has('pre_obs')?' has-error':'' !!}">
-						{!! Form::label('pre_obs', 'Motivo') !!}
-						{!! Form::textarea('pre_obs', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el motivo de la salida ya sea comisión, personal u otros','rows' => 4]) !!}
-						@if($errors->has('pre_obs'))
+						{!! Form::label('pre_motivo', 'Motivo del Permiso') !!}
+						{!! Form::textarea('pre_motivo', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el motivo de la salida ya sea comisión, personal u otros','rows' => 4]) !!}
+						@if($errors->has('pre_motivo'))
 							<span style="color:red;">
-								<strong>{!! $errors->first('pre_obs') !!}</strong>
+								<strong>{!! $errors->first('pre_motivo') !!}</strong>
 							</span>
 						@endif
                     </div>
 				</div>
+				<div class="col-md-1"></div>
 			</div>
 		</div>
 	</div>
